7e71559153e69c7c30675521962f70242d61f702
JiiMakinen <44932698+JiiMakinen@users.noreply.github.com>
2019-02-06 10:25:17 +0200

    imported packages, read files

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-#Hello world
-print("Hello world!")
+
+# for data analysis
+import pandas as pd
+import numpy as np
+
+
+# for visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+#import the train and test files (CSV)
+
+test = pd.read_csv('test.csv')
+train = pd.read_csv('train.csv')
+
+print(train.describe(include="all"))
+
+#11 rows x 12 columns, a lot of NaN values
+
+
+
+
